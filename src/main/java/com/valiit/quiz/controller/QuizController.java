dae514938ee62c8a51e64b81149a409465fce787
Kristjan95 <kristjan.tepper@hotmail.com>
2023-10-18 16:08:08 +0300

    Uploading base files

---
+++ src/main/java/com/valiit/quiz/controller/QuizController.java
@@ -0,0 +1,22 @@
+package com.valiit.quiz.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class QuizController {
+
+    @GetMapping("/api/quiz")
+    public Map<String, String> demo () {
+        return Map.of("message", "It is Wednesday!");
+    }
+
+    @PostMapping("/api/quiz")
+    public void demoPost (@RequestBody Map<String, String> body) {
+        System.out.println(body.get("email"));
+    }
+}
