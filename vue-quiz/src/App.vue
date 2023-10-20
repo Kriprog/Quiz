456a391febc3d36ae5e921ca6d425589f82b6acf
Laura Pall <palllaura.tln@gmail.com>
2023-10-20 09:46:32 +0300

    Added new icon

--- vue-quiz/src/App.vue
+++ vue-quiz/src/App.vue
@@ -8,11 +8,6 @@ import HelloWorld from './components/HelloWorld.vue'
    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
    -->
 
-
-
-
-
-
   </header>
   <RouterView />
 </template>
