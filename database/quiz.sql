9a02b904b123368f6d7d5b0b028aba41c7c5057f
Lauri Valdre <bastardhedgehog@gmail.com>
2023-10-23 09:13:38 +0300

    StartQuestionsPage and QuestionsPage and Router change

--- database/quiz.sql
+++ database/quiz.sql
@@ -13,4 +13,6 @@ create table User_account
 insert into User_account (name, password, email, highscore)
 values ('TestKasutaja', 'Salajane', 'test@mail.ee', 0);
 
+
 ALTER USER postgres WITH PASSWORD 'postgres';
+select * from User_account
