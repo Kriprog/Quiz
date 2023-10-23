eef78f67897a42ba10c96e6c5733a836340bf715
Kristjan95 <kristjan.tepper@hotmail.com>
2023-10-23 10:55:13 +0300

    Register function

--- vue-quiz/src/components/QuestionsPage.vue
+++ vue-quiz/src/components/QuestionsPage.vue
@@ -1,10 +1,16 @@
 <script setup>
 
+import Header from "@/components/Header.vue";
 </script>
 
 <template>
   <div>
-    <h1>QuestionsPage</h1>
+    <Header />
+    <header class="bg-white shadow">
+      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">QuestionsPage</h1>
+      </div>
+    </header>
     <!-- Add your content here -->
   </div>
 </template>
--- vue-quiz/src/views/AboutView.vue
+++ vue-quiz/src/views/AboutView.vue
@@ -1,52 +1,19 @@
-<script setup>
+<script>
+import Header from '../components/Header.vue';
 
-import {ref} from "vue";
-
-let message = ref("Hello!")
-let emailField = ref()
-
-async function fetchData() {
-  const response = await fetch('/api/quiz')
-  const data = await response.json()
-  message.value = data.message
-
+export default {
+  components: {
+    Header,
+  },
+  // Other component options
 }
-async function send() {
-  const data = {
-    email: emailField.value
-  }
-  const response = await fetch("/api/quiz", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(data)
-
-  });
-
-}
-fetchData()
-
 </script>
-
 <template>
-  <div class="about">
-    <h1>{{ message }}</h1>
-    <h1>Test</h1>
-    <div>
-      <label for="email" class="block text-sm font-medium leading-6 text-white-900">Email</label>
-      <div class="mt-2">
-        <input v-model="emailField" type="email" name="email" id="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="you@example.com" />
-        <button @click="send">Send!</button>
-      </div>
+  <div>
+    <Header /> <!-- Include the Header component here -->
+    <!-- Other content of your page goes here -->
+    <div> <!-- This div contains your main content -->
+      <!-- Other content of your page goes here -->
     </div>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
