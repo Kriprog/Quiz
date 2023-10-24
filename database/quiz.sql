b2fcd1f6dfde129e3b85ffd0baed72c4a76ca7ee
Kristjan95 <kristjan.tepper@hotmail.com>
2023-10-24 14:24:24 +0300

    Question method, quiz front end.

--- database/quiz.sql
+++ database/quiz.sql
@@ -20,20 +20,20 @@ create table Questions
     option3        text not null
 );
 
+create table Sessions
+(
+    id              serial PRIMARY KEY,
+    session_token   varchar(255) not null,
+    user_id         int not null,
+    expiration_date timestamp not null,
+    created_at      timestamp default current_timestamp
+
+);
+
 insert into User_account (name, password, email, highscore)
 values ('TestKasutaja', 'Salajane', 'test@mail.ee', 0);
 
 INSERT INTO Questions (question_text, correct_answer, option1, option2, option3)
 VALUES ('What is the capital of France?', 'Paris', 'London', 'Berlin', 'Madrid'),
-       ('Which planet is known as the "Red Planet"?', 'Mars', 'Earth', 'Venus', 'Jupiter'),
-('What is the largest planet in our solar system?', 'Jupiter', 'Earth', 'Mars', 'Venus'),
-('Which gas do plants absorb from the atmosphere during photosynthesis?', 'Carbon Dioxide', 'Oxygen', 'Nitrogen', 'Hydrogen'),
-('Who wrote the play "Romeo and Juliet"?', 'William Shakespeare', 'Charles Dickens', 'Mark Twain', 'Jane Austen'),
-('What is the capital of France?', 'Paris', 'Berlin', 'London', 'Rome'),
-('Which animal is known as the "King of the Jungle"?', 'Lion', 'Elephant', 'Tiger', 'Giraffe'),
-('In which year did Christopher Columbus first arrive in the Americas?', '1492', '1620', '1776', '1865'),
-('What is the chemical symbol for water?', 'H2O', 'CO2', 'O2', 'N2'),
-('Who painted the Mona Lisa?', 'Leonardo da Vinci', 'Vincent van Gogh', 'Pablo Picasso', 'Rembrandt');
+       ('Which planet is known as the "Red Planet"?', 'Mars', 'Earth', 'Venus', 'Jupiter')
 ;
-
-
