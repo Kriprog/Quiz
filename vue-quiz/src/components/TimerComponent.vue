513bc6df7a2a2c8f4fdcb70033b8159086fd1ca6
Lauri Valdre <bastardhedgehog@gmail.com>
2023-10-31 16:09:01 +0200

    Lauri changed Timer functionality on Tuesday.

--- vue-quiz/src/components/TimerComponent.vue
+++ vue-quiz/src/components/TimerComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="timer">
+  <div class="timerComponent">
     {{ timeLeft }}
   </div>
 </template>
@@ -9,31 +9,42 @@ export default {
   data() {
     return {
       timeLeft: 30, // Initialize the timer with 30 seconds
+      countdown: null, // Declare countdown as an instance variable
     };
   },
   methods: {
     startTimer() {
-      const countdown = setInterval(() => {
-        if (this.timeLeft > 0) {
-          this.timeLeft -= 1;
-        } else {
-          clearInterval(countdown); // Stop the timer when timeLeft reaches 0
-          this.$emit('timerExpired'); // Emit an event when the timer expires
-        }
-      }, 1000); // Update the timer every 1 second
+      if (this.countdown === null) { // Check if the timer is not already running
+        this.countdown = setInterval(() => {
+          if (this.timeLeft > 0) {
+            this.timeLeft -= 1;
+          } else {
+            clearInterval(this.countdown); // Stop the timer when timeLeft reaches 0
+            this.$emit('timerExpired'); // Emit an event when the timer expires
+          }
+        }, 1000); // Update the timer every 1 second
+      }
+    },
+    stopTimer() {
+      clearInterval(this.countdown); // Stop the timer using the instance variable
+      this.countdown = null; // Reset the countdown variable
+    },
+    resetTimer() {
+      this.timeLeft = 30; // Reset the timer to 30 seconds
     },
   },
   mounted() {
     this.startTimer(); // Start the timer when the component is mounted
   },
   beforeDestroy() {
-    // Clean up the timer when the component is destroyed
+    clearInterval(this.countdown); // Clean up the timer when the component is destroyed
   },
 };
 </script>
 
+
 <style scoped>
-.timer {
+.timerComponent {
   font-size: 36px; /* Adjust the font size to your preference */
   font-weight: bold; /* Optional: Make the numbers stand out */
 }
